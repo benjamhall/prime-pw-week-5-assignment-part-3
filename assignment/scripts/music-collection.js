04cf4948fb4775907ec3fa6c8073b4c692b78b6f
Ben Hall <benjamhall@gmail.com>
2021-05-01 18:13:09 -0500

    add album3

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -9,18 +9,25 @@ function addCollection (album){      // Function to add album to the collection
   console.log('album has been added');
   return true;
 }
-console.log('Add to Collection');
+console.log('Add to the Album Collection');
 
-let album1 = {
+let album1={
     title: 'The Beatles',
     artist: 'The Beatles',
     yearPublished: 1968,
   };
-console.log('The Beatles album:', album1);
+console.log('The Beatles by the Beatles:', album1);
 
-let album2 = {
+let album2={
     title: 'Let it Be',
     artist: 'The Beatles',
     yearPublished: 1970,
 };
-console.log('Let it Be album:', album2);
+console.log('Let it Be by the Beatles:', album2);
+
+let album3={
+    title: 'Sunflower',
+    artist: 'The Beach Boys',
+    yearPublished: 1970,
+};
+console.log('Sunflower by The Beach Boys:', album3);
