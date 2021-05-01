f15fa531621889db3854fdc07e7121252dae5105
Ben Hall <benjamhall@gmail.com>
2021-05-01 18:02:56 -0500

    add album1

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,5 +1,24 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+let collection =[];
 
-addCollection
+
+function addCollection (album){      // Function to add album to the collection
+  console.log('in addCollection:', album);
+  collection.push(album);
+  console.log('album has been added');
+  return true;
+}
+console.log('addCollection works');
+
+let album1 = {
+    title: 'The Beatles',
+    artist: 'The Beatles',
+    yearPublished: 1968,
+  };
+console.log('The Beatles album:', album1);
+
+let album2 = {
+    title: ''
+    artist: ''
+}
